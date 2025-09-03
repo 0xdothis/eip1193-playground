6133ec8f450585b8cafc55d81e43f79c18173c69
s-olagoke <olagokesikiru06@gmail.com>
2025-09-03 02:20:15 +0100

    chore: A simple playground to understand EIP-1193

---
+++ src/vite-env.d.ts
@@ -0,0 +1,28 @@
+/// <reference types="vite/client" />
+
+//
+interface ProvideMessage {
+  readonly type: string;
+  readonly data: unknown;
+}
+
+// connected wallet chainId type
+interface ProviderConnectionInfo {
+  readonly chainId: string;
+}
+
+interface EthereumProvider {
+  request: (args: { method: string; params?: unknown[] }) => Promise<unknown>;
+  on?: (eventName: string, listeners: (...args: unknown[]) => void) => void;
+  removeListener?: (
+    eventName: string,
+    listeners: (...args: unknown[]) => void,
+  ) => void;
+  isMetamask?: boolean;
+  providers?: EthereumProvider[];
+  chainId?: ProviderConnectionInfo;
+}
+
+interface ProviderMethods {
+  methods: string[];
+}
